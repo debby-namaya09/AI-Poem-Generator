214ced981d2715d639bc531c92595a938d03409d
debby-namaya09 <153802138+debby-namaya09@users.noreply.github.com>
2026-07-23 22:14:15 +0200

    added basic javascript and typewriter function

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-console.log("website loeded")
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
