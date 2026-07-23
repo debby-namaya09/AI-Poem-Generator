bd77997935bff1ff86ab8dadaf0b1279029f7427
debby-namaya09 <153802138+debby-namaya09@users.noreply.github.com>
2026-07-23 23:40:59 +0200

    improve AI response for user experience

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "094b9b793aea03ec7botf2b3b2d837f5";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 to 6 lines poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem  and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
